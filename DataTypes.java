f16884424d18394176920ab00efd46c98005a1be
KPriya07 <125984720+KPriya07@users.noreply.github.com>
2023-02-22 15:23:18 +0000

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+class DataTypes
+{
+public static void main(String args[])
+{
+byte a=2;
+short b=5;
+long c=10000000000L;
+long i=10000000000L;
+long z=c+i;
+long d=100L; 
+float e=2.5f;
+double f=100.5;
+boolean g=true;
+char h='P';
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+System.out.println(z);
+}
+}
