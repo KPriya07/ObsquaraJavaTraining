00dbd120ac5241993cf63371627a69499d4c4718
Admin <Admin@192.168.1.203>
2023-03-08 15:27:46 +0000

    Commit to Github

---
+++ MavenPrograms/src/main/java/EncapDemo.java
@@ -0,0 +1,14 @@
+
+public class EncapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1=new Student();
+		s1.setId(101);
+		System.out.println(s1.getId());
+		s1.setName("Priya");
+		System.out.println(s1.getName());
+	}
+
+}
---
+++ MavenPrograms/src/main/java/Student.java
@@ -0,0 +1,27 @@
+
+public class Student {
+
+	
+	private int id;
+	private String name;
+	
+	public int getId() { //getter method
+		
+		return id;
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public void setId(int id) { //setter method
+		
+	this.id=id;
+	}
+	
+	public void setName(String name) {
+		
+		this.name=name;
+	}
+}
